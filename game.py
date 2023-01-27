26993e7b63c09088516379e38cd4d651a1a51877
Carson Bush <hyperdriveguy@gmail.com>
2023-01-27 16:31:59 -0700

    Use multicasting

--- game.py
+++ game.py
@@ -11,18 +11,18 @@ def load_game_data(file_path):
     return game_data
 
 # IP helper function - https://stackoverflow.com/a/28950776
-def get_ip():
+def get_machine_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(0)
     try:
         # doesn't even have to be reachable
         s.connect(('10.254.254.254', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
+        ip = s.getsockname()[0]
+    except OSError:
+        ip = '127.0.0.1'
     finally:
         s.close()
-    return IP
+    return ip
 
 
 
@@ -30,9 +30,11 @@ class P2PNode:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.ip = get_machine_ip()
         self.tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcpsock.bind((self.host, self.port))
         self.udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udpsock.bind(('0.0.0.0', self.port))
         self.games_list = []
 
     def listen(self):
@@ -45,13 +47,14 @@ class P2PNode:
             connection_thread.start()
 
     def udp_listen(self):
-        self.udpsock.bind(('', self.port))
+        self.udpsock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("224.0.0.1") + socket.inet_aton("0.0.0.0"))
         while True:
             data, addr = self.udpsock.recvfrom(1024)
             print(f"Received broadcast from {addr}")
-            print(data, addr)
+            print(addr, 'reports:', data)
             if "game_online" in data.decode():
                 self.games_list.append(addr)
+
 
     def connect(self, host, port):
         try:
@@ -64,7 +67,7 @@ class P2PNode:
                     break
                 print(f"Received: {data}")
             self.tcpsock.close()
-        except Exception as e:
+        except OSError as e:
             print(f"Error: {e}")
             # remove the peer from the games_list
             self.games_list.remove((host, port))
@@ -74,28 +77,18 @@ class P2PNode:
                 print(f"Connecting to peer at {peer[0]}:{peer[1]}")
                 self.connect(peer[0], peer[1])
 
-
-
-
     def broadcast(self):
+        group = ('224.0.0.1', self.port)
+        self.udpsock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
         while True:
-            try:
-                ip = ipaddress.IPv4Address(socket.gethostbyname(socket.gethostname()))
-                print(ip)
-                if str(ip) in ('127.0.0.1', '127.0.1.1'):
-                    ip = ipaddress.IPv4Address(socket.gethostbyname(socket.gethostname() + '.local'))
-                print(ip)
-                net = ipaddress.IPv4Network(ip)
-                broadcast_ip = str(net.broadcast_address)
-                self.udpsock.sendto(f"game_online".encode(), (broadcast_ip, self.port))
-                print(f"Broadcasting to {broadcast_ip}:{self.port}")
-            except Exception as e:
-                print(f"Error: {e}")
+            self.udpsock.sendto("game_online".encode(), group)
+            print(f"Broadcasting to {group}")
             sleep(5) # wait 5 seconds before trying again
 
 
+
 if __name__ == '__main__':
-    node = P2PNode("0.0.0.0", 1555)
+    node = P2PNode("0.0.0.0", 44000)
     broadcast_thread = threading.Thread(target=node.broadcast)
     listen_thread = threading.Thread(target=node.udp_listen)
     broadcast_thread.start()
