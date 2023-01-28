7c3238e2f0713c05c9daed63185a14635dcf69f2
Carson Bush <hyperdriveguy@gmail.com>
2023-01-27 18:46:30 -0700

    TCP and UDP on different ports

--- game.py
+++ game.py
@@ -6,7 +6,8 @@ import ipaddress
 from time import sleep
 
 MULTICAST_ADDR = '224.0.0.10'
-GAME_PORT = 44001
+BROADCAST_PORT = 44000
+TCP_PORT = BROADCAST_PORT + 1
 
 def load_game_data(file_path):
     with open(file_path, 'r') as f:
@@ -37,12 +38,12 @@ class P2PNode:
         self.tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcpsock.bind((self.host, self.port))
         self.udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udpsock.bind(('0.0.0.0', self.port))
+        self.udpsock.bind(('0.0.0.0', BROADCAST_PORT))
         self.games_list = []
 
     def listen(self):
         self.tcpsock.listen()
-        print(f"Listening for incoming connections on {self.host}:{self.port}")
+        print(f"Listening for incoming connections on {self.host}:{TCP_PORT}")
         while True:
             client, addr = self.tcpsock.accept()
             client.send("Welcome to the P2P network!".encode())
@@ -63,20 +64,21 @@ class P2PNode:
                 print(f"Ignoring broadcast message from localhost {sender_addr}")
 
     def connect(self, host, port):
-        try:
-            self.tcpsock.connect((host, port))
-            print(f"Connected to {host}:{port}")
-            self.tcpsock.send(f"Hello from {self.host}:{self.port}".encode())
-            while True:
-                data = self.tcpsock.recv(1024).decode()
-                if not data:
-                    break
-                print(f"Received: {data}")
-            self.tcpsock.close()
-        except OSError as e:
-            print(f"Error: {e}")
+        # try:
+        self.tcpsock.connect((host, port))
+        print(f"Connected to {host}:{port}")
+        self.tcpsock.send(f"Hello from {self.host}:{self.port}".encode())
+        while True:
+            data = self.tcpsock.recv(1024).decode()
+            if not data:
+                break
+            print(f"Received: {data}")
+        self.tcpsock.close()
+        # except ConnectionRefusedError as e:
+        # except OSError as e:
+            # print(f"Error: {e}")
             # remove the peer from the games_list
-            self.games_list.remove(host)
+            # self.games_list.remove((host)
             # try connecting to another peer
             # if self.games_list:
             #     peer = random.choice(self.games_list)
@@ -84,7 +86,7 @@ class P2PNode:
             #     self.connect(peer[0], peer[1])
 
     def broadcast(self):
-        group = (MULTICAST_ADDR, self.port)
+        group = (MULTICAST_ADDR, BROADCAST_PORT)
         self.udpsock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
         while True:
             self.udpsock.sendto("game_online".encode(), group)
@@ -94,7 +96,7 @@ class P2PNode:
 
 
 if __name__ == '__main__':
-    node = P2PNode("0.0.0.0", GAME_PORT)
+    node = P2PNode("0.0.0.0", BROADCAST_PORT)
     broadcast_thread = threading.Thread(target=node.broadcast)
     listen_thread = threading.Thread(target=node.udp_listen)
     broadcast_thread.start()
@@ -102,7 +104,7 @@ if __name__ == '__main__':
     while True:
         if node.games_list:
             peer = random.choice(node.games_list)
-            print(f"Connecting to peer at {peer}:{GAME_PORT}")
+            print(f"Connecting to peer at {peer}:{TCP_PORT}")
             node.connect(peer, GAME_PORT)
             # break
         sleep(1)
